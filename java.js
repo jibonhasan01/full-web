1a103ca5a5e13616137b7ff0df2fdb2b99aeba3a
Jibon Mia <98581144+jibonhasan01@users.noreply.github.com>
2022-02-12 08:09:30 +0600

    added
    aeed

---
+++ java.js
@@ -0,0 +1,24 @@
+
+var button1=document.querySelector(".button1");
+var button2=document.querySelector(".button2");
+button1.addEventListener("click",lll);
+
+function lll(){
+document.querySelector("#p").innerHTML=" Gmail: jibonhasano199@gmail.com";}
+
+button2.addEventListener("click",function(){
+    alert(" i have'nt CV");
+});
+
+document.querySelectorAll("ul li")[0].addEventListener("click",function(){
+    alert("sorry. we have no link")
+})
+document.querySelectorAll("ul li")[1].addEventListener("click",function(){
+    alert("sorry. we have no link")
+})
+document.querySelectorAll("ul li")[2].addEventListener("click",function(){
+    alert("sorry. we have no link")
+})
+document.querySelectorAll("ul li")[3].addEventListener("click",function(){
+    alert("sorry. we have no link")
+})
